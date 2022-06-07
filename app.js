70f4a76c4cf5da28a9ad502e07aab3c8165dabac
Aman Singh <imaman07singh@gmail.com>
2022-06-07 15:46:54 +0530

    CORS issues

--- app.js
+++ app.js
@@ -5,22 +5,16 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors')
 
+app.use(cors({
+  origin: "*"
+}))
 app.use(bodyParser.json());
-app.use(cors())
 
 
 // Connect to DB
 mongoose.connect(process.env.DB_URI)
   .then((data) => console.log(`Connected with DB: ${data.connection.host}`))
   .catch((err) => console.log('Error in DB Connection', err));
-
-// // Create bucket
-// mongoose.connection.on("connected", () => {
-//   var db = mongoose.connection.db
-//   global.bucket = new mongoose.mongo.GridFSBucket(db, {
-//     bucketName: "uploads"
-//   });
-// })
 
 
 //Routes
