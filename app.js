0dafe2fc2ac5ee1a79d88b44af29e823f6a73f8e
Aman Singh <imaman07singh@gmail.com>
2022-06-07 15:56:04 +0530

    setHeader for cors

--- app.js
+++ app.js
@@ -5,10 +5,21 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors')
 
-app.use(cors({
-  origin: "*"
-}))
 app.use(bodyParser.json());
+
+// app.use(cors({
+//   origin: "*"
+// }))
+
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
 
 
 // Connect to DB
