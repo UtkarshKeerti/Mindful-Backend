308c436d17d6828054dd746698cf68cdd14a9e96
Aman Singh <imaman07singh@gmail.com>
2022-06-07 16:18:55 +0530

    env PORT

--- app.js
+++ app.js
@@ -6,20 +6,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 
 app.use(bodyParser.json());
-
-// app.use(cors({
-//   origin: "*"
-// }))
-
-app.use(function (req, res, next) {
-
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  next();
-});
+app.use(cors());
 
 
 // Connect to DB
@@ -33,4 +20,4 @@ app.use('/api', require('./src/routers/api'));
 
 
 // Listening to the server
-app.listen(process.env.SERVER_PORT, () => console.log('Server listening on port: ', process.env.SERVER_PORT))
+app.listen(process.env.PORT || 2020, () => console.log('Server listening on port: ', process.env.PORT))
