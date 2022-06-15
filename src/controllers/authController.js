d7ab1e731de4751d22fc97c9e1fb9ae5725da1c6
Aman Singh <imaman07singh@gmail.com>
2022-06-15 11:24:11 +0530

    Login Error messages

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -9,7 +9,7 @@ exports.registerUser = async (req, res) => {
 
   // Check for existing email id
   const emailExists = await Users.findOne({ email: req.body.email });
-  if (emailExists) return res.status(400).json('Email already exists!');
+  if (emailExists) return res.status(400).json({ message: "Email already exists" });
 
   const hashedPass = await bcrypt.hash(req.body.password, 10)
 
@@ -32,14 +32,16 @@ exports.adminLogin = async (req, res) => {
   try {
     // Check if the user exists
     const user = await Users.findOne({ email: req.body.email })
-    if (!user) return res.status(400).json("This email doesn't exist!")
+
+    // --- Status is 200 for these error messages so that frontend can display them. --- //
+    if (!user) return res.status(200).json({ message: "This email doesn't exist!" })
 
     // Check if the user is admin
-    if (!user.isAdmin) return res.status(400).json("This user is not an admin!")
+    if (!user.isAdmin) return res.status(200).json({ message: "This user is not an admin!" })
 
     // verify password
     const passCheck = await bcrypt.compare(req.body.password, user.password)
-    if (!passCheck) return res.status(400).json("Invalid Password!");
+    if (!passCheck) return res.status(200).json({ message: "Invalid Password!" });
 
     // Generate token
     const authToken = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET)
@@ -62,11 +64,11 @@ exports.loginUser = async (req, res) => {
   try {
     // Check if the user exists
     const user = await Users.findOne({ email: req.body.email })
-    if (!user) return res.status(400).json("This Email doesn't exist!")
+    if (!user) return res.status(400).json({ message: "This email doesn't exist!" })
 
     // verify password
     const passCheck = await bcrypt.compare(req.body.password, user.password)
-    if (!passCheck) return res.status(400).json("Invalid Password!");
+    if (!passCheck) return res.status(400).json({ message: "Invalid Password!" });
 
     // Generate token
     const authToken = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET)
